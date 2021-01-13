64eb8c329dd67469b9afae1b0f56a29bf0743bde
Aahana-C <aahanacha@gmail.com>
2021-01-13 17:03:53 +0000

    added

--- sketch.js
+++ sketch.js
@@ -5,11 +5,19 @@ var playerCount;
 var allPlayers;
 var distance = 0;
 var database;
-
 var form, player, game;
-
 var cars, car1, car2, car3, car4;
+var car1Image, car2Image, car3Image, car4Image, trackImage, groundImage;
 
+function preload(){
+  car1Image = loadImage("images/car1.png");
+  car2Image = loadImage("images/car2.png");
+  car3Image = loadImage("images/car3.png");
+  car4Image = loadImage("images/car4.png");
+  trackImage = loadImage("images/track.png");
+  groundImage = loadImage("images/ground.png");
+  
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -28,4 +36,7 @@ function draw(){
     clear();
     game.play();
   }
+if(gameState === 2){
+game.end();
+}
 }
